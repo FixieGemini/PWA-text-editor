5e106cf6e599fdd7b4e02d472b6480f984d44ed9
FixieGemini <109640240+FixieGemini@users.noreply.github.com>
2023-01-15 14:43:49 -0700

    fixed webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // export const putDb = async (content) => console.error('putDb not implemented');
-  export const putDB = async (content) => {
+  export const putDb = async (content) => {
     console.log('PUT into database');
 
     // Create connection to database and version to be used
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,81 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+
+// // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// // TODO: Add CSS loaders and babel to webpack.
+
+// module.exports = () => {
+//   return {
+//     mode: 'development',
+//     entry: {
+//       main: './src/js/index.js',
+//       install: './src/js/install.js'
+//     },
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//     plugins: [
+//       // Webpack plugin generates html file and injects bundles
+//       new HtmlWebpackPlugin({
+//         template: './index.html',
+//         title: 'JATE'
+//       }),
+//       // Injects custom service worker
+//       new InjectManifest({
+//         swSrc: './src-sw.js',
+//         swDest: 'src-sw.js',
+//       }),
+//       // Creates a manifest.json file
+//       new WebpackPwaManifest({
+//         fingerprints: false,
+//         inject: true,
+//         name: 'Just Another Text Editor',
+//         short_name: 'JATE',
+//         description: 'PWA text editor',
+//         background_color: '#225ca3',
+//         theme_color: '#225ca3',
+//         start_url: '/',
+//         publicPath: '/',
+//         icons: [
+//           {
+//             src: path.resolve('src/images/logo.png'),
+//             sizes: [96, 128, 192, 256, 384, 512],
+//             destination: path.join('assets', 'icons'),
+//           },
+//         ],
+//       }),
+//     ],
+
+//     module: {
+//       // CSS Loaders
+//       rules: [
+//         {
+//           test: /\.css$/i,
+//           use: ['style-loader', 'css-loader'],
+//         },
+//         {
+//           test: /\.m?js$/,
+//           exclude: /node_modules/,
+//           // Babel loader to use ES6
+//           use: {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: ['@babel/preset-env'],
+//               plugins: ['@babel/plugin-proposal-obect-rest-spread', '@babel/transform-runtime'],
+//             },
+//           },
+//         },
+//       ],
+//     },
+//   };
+// };
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -18,23 +89,20 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // Webpack plugin generates html file and injects bundles
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'JATE'
+        title: 'J.A.T.E'
       }),
-      // Injects custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      // Creates a manifest.json file
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
         name: 'Just Another Text Editor',
-        short_name: 'JATE',
-        description: 'PWA text editor',
+        short_name: 'J.A.T.E',
+        description: 'Takes notes with JavaScript syntax highlighting!',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
@@ -50,7 +118,6 @@ module.exports = () => {
     ],
 
     module: {
-      // CSS Loaders
       rules: [
         {
           test: /\.css$/i,
@@ -59,12 +126,11 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // Babel loader to use ES6
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-obect-rest-spread', '@babel/transform-runtime'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
